c9aaed48457cbb119b3df0529819a4d881ed7c0b
Wyatt Gorman <wyattgorman@google.com>
2021-04-16 17:34:10 +0000

    Fix issue running disable twice disabling extra cores.

--- manage_hyperthreading.sh
+++ manage_hyperthreading.sh
@@ -64,7 +64,7 @@ function enable(){
 }
 	
 function disable(){
-	for vcpu in `cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | cut -d, -f2 | cut -d- -f2 | sort -u`; do
+	for vcpu in `cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | cut -s -d, -f2 | cut -d- -f2 | sort -u`; do
 		echo 0 > /sys/devices/system/cpu/cpu$vcpu/online
 	done
 }
